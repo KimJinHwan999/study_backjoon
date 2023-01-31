095218eb08894ec44cea9588e88939d46c1dce01
KimJinHwan999 <wlsghks5438@gmail.com>
2023-02-01 02:45:00 +0900

    0201 v2

--- oneDimensionalArray/q10871.java
+++ oneDimensionalArray/q10871.java
@@ -9,29 +9,32 @@ import java.util.StringTokenizer;
 
 public class q10871 {
     public static void main(String[] args) throws IOException{
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringTokenizer st_ = new StringTokenizer(br.readLine());
-        
+        StringTokenizer st = null;
+        StringTokenizer st_ = null;
+
+        st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int X = Integer.parseInt(st.nextToken());
-        int[] arr = new int[X - 1];
+        String[] arr = new String[X - 1];
         int count = 0;
-
+        
+        st_ = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++){
+
             int A = Integer.parseInt(st_.nextToken());
+
             if(A < X){
-                arr[count] = A;
+                arr[count] = A + " ";
                 count++; 
             }
         }
-        
+
         br.close();
 
-        for(int i = 0; i < X - 1; i++){
-            bw.write(String.valueOf(arr[i]));
+        for(int i = 0; i < count; i++){
+            bw.write(arr[i]);
         }
         
         bw.flush();
