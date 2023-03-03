274fef7dd300f1b648b12d24448ad3e255da6621
KimJinHwan999 <wlsghks5438@gmail.com>
2023-03-03 15:48:46 +0900

    0303 v1

---
+++ arithmeticOperations/q11382.java
@@ -0,0 +1,18 @@
+package arithmeticOperations;
+
+import java.util.Scanner;
+
+public class q11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Long A = sc.nextLong();
+        Long B = sc.nextLong();
+        Long C = sc.nextLong();
+
+        sc.close();
+
+        System.out.println(A+B+C);
+
+    }
+}
