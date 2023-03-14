e8fc79a92d89ebf78f58e8e0ce093c7d2b000e6e
KimJinHwan999 <wlsghks5438@gmail.com>
2023-03-14 16:49:04 +0900

    0314v1

--- string/q10809.java
+++ string/q10809.java
@@ -8,41 +8,26 @@ public class q10809 {
 
         String S = sc.next();
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        int[] cnt = new int[S.length()];
         int[] answer = new int[26];
-
+        
         for(int i = 0; i < 26; i++){
             answer[i] = -1;
         }
 
         for(int i = 0; i < S.length(); i++){
-            int j = 0;
-            while(cnt[i] != -1){
-                if(S.charAt(i) != alphabet.charAt(j)){
-                    j++;
-                }
-
+            for(int j = 0; j < 26; j++){
                 if(S.charAt(i) == alphabet.charAt(j)){
-                    cnt[i] = j;
+                    if(answer[j] != -1){
+                        break;
+                    }
+                    answer[j] = i;
                     break;
                 }
-
             }
-            // for(int j = 0; j < 26; j++){
-            //     if(S.charAt(i) == alphabet.charAt(j)){
-            //         cnt[i] = j;
-            //     }
-
-            // }
-        }
-
-        for(int i = 0; i < S.length(); i++){
-            answer[cnt[i]] = i;
         }
 
         for(int i = 0; i < 26; i++){
             System.out.print(answer[i] + " ");
         }
-
     }    
 }
